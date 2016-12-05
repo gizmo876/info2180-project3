3b759808c2d3a43b3c0a05bcc8992c4fb7b4485b
gizmo876 <theshvk@gmail.com>
2016-12-04 22:06:45 -0500

    Create schema.php

---
+++ schema.php
@@ -0,0 +1,36 @@
+<?php
+
+	$username = getenv('');
+	$password = '';
+	$database = 'schema.sql';
+	$query = $_POST['ID'];
+	$querysms = $_POST['message'];
+
+
+
+class cheapomail
+{
+
+	var $username;
+	var $password;
+	var $database;
+	var $tablename;
+
+	 function schema()
+	{
+		$this->sitename = 'cheapomail.com';
+		$this->
+	} 
+		
+	//--------------------------------------------INITIALIZATION OF THE DATABASE-------------------------------------------------\\  
+	 function database($user,$pass,$database,$tablename)
+    {
+        $this->username = $username;
+        $this->password  = $password;
+        $this->database  = $database;
+        $this->tablename = $tablename;
+    }
+
+
+}	
+?>
